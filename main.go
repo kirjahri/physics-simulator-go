0cae479aa45ce7831c0c5dd4772903025f5b5f82
kirjahri <150558369+kirjahri@users.noreply.github.com>
2025-07-06 16:56:00 -0400

    feat: create blue balls where ever the mouse clicks
    feat: add `balls` array to `Game` struct
    feat: change background color to white

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"math"
+	"math/rand"
 
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
@@ -15,7 +16,7 @@ type Ball struct {
 }
 
 type Game struct {
-	ball Ball
+	balls []Ball
 }
 
 const (
@@ -23,48 +24,57 @@ const (
 	screenHeight = 450
 	gravity      = 9.8
 	multiplier   = 1
-	friction     = 0.85
+	friction     = 0.7
 	minimum      = 55
+	minBallSize  = 5
+	maxBallSize  = 25
 )
 
-func (g *Game) Init() {
-	g.ball = Ball{
-		position: rl.NewVector2(float32(screenWidth/2), float32(screenHeight/2)),
-		radius:   20,
-		color:    rl.White,
-	}
-}
+func (g *Game) Init() {}
 
 func (g *Game) Update() {
-	if !g.ball.isStill {
-		deltaTime := rl.GetFrameTime()
-		threshold := screenHeight - g.ball.radius
+	deltaTime := rl.GetFrameTime()
 
-		g.ball.velocity += gravity * multiplier
-		newY := g.ball.position.Y + g.ball.velocity*deltaTime
+	if rl.IsMouseButtonPressed(rl.MouseButtonLeft) {
+		g.balls = append(g.balls, Ball{
+			position: rl.GetMousePosition(),
+			radius:   minBallSize + rand.Float32()*(maxBallSize+1-minBallSize),
+			color:    rl.Blue,
+		})
+	}
 
-		if newY >= threshold {
-			// Correct the ball's position by subtracting how far it "penetrates" into the ground
-			penetration := newY - threshold
-			g.ball.position.Y = threshold - penetration
+	for i, b := range g.balls {
+		if !b.isStill {
+			threshold := screenHeight - b.radius
 
-			// If the ball's speed and distance to the ground are low enough, stop it completely
-			// This prevents the ball from jittering when close to the ground
-			if math.Abs(float64(g.ball.velocity)) < minimum && math.Abs(float64(g.ball.position.Y-threshold)) < minimum {
-				g.ball.velocity = 0
-				g.ball.position.Y = threshold
-				g.ball.isStill = true
+			g.balls[i].velocity += gravity * multiplier
+			newY := g.balls[i].position.Y + g.balls[i].velocity*deltaTime
+
+			if newY >= threshold {
+				// Correct the ball's position by subtracting how far it "penetrates" into the ground
+				penetration := newY - threshold
+				g.balls[i].position.Y = threshold - penetration
+
+				// If the ball's speed and distance to the ground are low enough, stop it completely
+				// This prevents the ball from jittering when close to the ground
+				if math.Abs(float64(g.balls[i].velocity)) < minimum && math.Abs(float64(g.balls[i].position.Y-threshold)) < minimum {
+					g.balls[i].velocity = 0
+					g.balls[i].position.Y = threshold
+					g.balls[i].isStill = true
+				} else {
+					g.balls[i].velocity *= -friction
+				}
 			} else {
-				g.ball.velocity *= -friction
+				g.balls[i].position.Y = newY
 			}
-		} else {
-			g.ball.position.Y = newY
 		}
 	}
 }
 
 func (g *Game) Draw() {
-	rl.DrawCircleV(g.ball.position, g.ball.radius, g.ball.color)
+	for _, b := range g.balls {
+		rl.DrawCircleV(b.position, b.radius, b.color)
+	}
 }
 
 func main() {
@@ -83,7 +93,7 @@ func main() {
 
 		rl.BeginDrawing()
 
-		rl.ClearBackground(rl.Black)
+		rl.ClearBackground(rl.White)
 		game.Draw()
 
 		rl.EndDrawing()
