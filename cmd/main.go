3acacbd067f5a08d0a4272ce95d96681d2adae4a
kirjahri <150558369+kirjahri@users.noreply.github.com>
2025-07-09 10:33:57 -0400

    feat: create elastic collisions
    refactor: `SetNewPosition` -> `Update` on `Ball` struct
    
    fix: edge collision detection

--- cmd/main.go
+++ cmd/main.go
@@ -34,26 +34,69 @@ const (
 	maxBallVel   = 5
 )
 
-func (b *Ball) SetNewPosition(dt float32) {
+func (b *Ball) Update() {
+	dt := rl.GetFrameTime()
 	b.velocity = rl.Vector2Add(b.velocity, rl.Vector2Scale(b.acceleration, dt))
 	b.position = rl.Vector2Add(b.position, rl.Vector2Scale(b.velocity, dt))
 }
 
 func (b *Ball) CheckEdgeCollision() {
-	if b.position.X >= screenWidth-b.radius || b.position.X <= b.radius {
+	if b.position.X > screenWidth-b.radius {
+		b.position.X = screenWidth - b.radius
+		b.velocity.X *= -1
+	} else if b.position.X < b.radius {
+		b.position.X = b.radius
 		b.velocity.X *= -1
 	}
 
-	if b.position.Y >= screenHeight-b.radius || b.position.Y <= b.radius {
+	if b.position.Y > screenHeight-b.radius {
+		b.position.Y = screenHeight - b.radius
 		b.velocity.Y *= -1
+	} else if b.position.Y < b.radius {
+		b.position.Y = b.radius
+		b.velocity.Y *= -1
+	}
+}
+
+func (b *Ball) CheckBallCollision(otherBall *Ball) {
+	impactVector := rl.Vector2Subtract(otherBall.position, b.position)
+	distance := rl.Vector2Length(impactVector)
+
+	if distance < b.radius+otherBall.radius {
+		// Correct the overlap when the balls collide
+		overlap := distance - (b.radius + otherBall.radius)
+		direction := rl.Vector2Scale(rl.Vector2Normalize(impactVector), overlap*0.5)
+		b.position = rl.Vector2Add(b.position, direction)
+		otherBall.position = rl.Vector2Subtract(otherBall.position, direction)
+
+		// Correct the distance
+		distance = b.radius + otherBall.radius
+		impactVector = rl.Vector2Scale(rl.Vector2Normalize(impactVector), distance)
+
+		massSum := b.mass + otherBall.mass
+		velDiff := rl.Vector2Subtract(otherBall.velocity, b.velocity)
+		num := rl.Vector2DotProduct(velDiff, impactVector)
+		den := massSum * distance * distance
+
+		// Ball 0 (b)
+		deltaVel0 := rl.Vector2Scale(impactVector, 2*otherBall.mass*num/den)
+		b.velocity = rl.Vector2Add(b.velocity, deltaVel0)
+
+		// Ball 1 (otherBall)
+		deltaVel1 := rl.Vector2Scale(impactVector, -2*b.mass*num/den)
+		otherBall.velocity = rl.Vector2Add(otherBall.velocity, deltaVel1)
+
+		// speed0 := rl.Vector2Length(b.velocity)
+		// speed1 := rl.Vector2Length(otherBall.velocity)
+		// kin0 := 0.5 * b.mass * speed0 * speed0
+		// kin1 := 0.5 * otherBall.mass * speed1 * speed1
+		// fmt.Println(kin0 + kin1)
 	}
 }
 
 func (g *Game) Init() {}
 
 func (g *Game) Update() {
-	dt := rl.GetFrameTime()
-
 	if rl.IsMouseButtonPressed(rl.MouseButtonLeft) {
 		mass := minBallMass + rand.Float32()*(maxBallMass+1-minBallMass)
 		g.balls = append(g.balls, Ball{
@@ -91,8 +134,16 @@ func (g *Game) Update() {
 		// 	}
 		// }
 
-		g.balls[i].SetNewPosition(dt)
+		g.balls[i].Update()
 		g.balls[i].CheckEdgeCollision()
+
+		for j := range g.balls {
+			if g.balls[i] == g.balls[j] {
+				continue
+			}
+
+			g.balls[i].CheckBallCollision(&g.balls[j])
+		}
 	}
 }
 
